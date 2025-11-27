9da0f08af1d25a3ef5992eaeab6158d0ab3fb56c
qingfeng-sys <gy930309@gmail.com>
2025-11-27 23:52:39 +0800

    添加筛选器，组织视图布局

--- catalog/admin.py
+++ catalog/admin.py
@@ -13,6 +13,8 @@ admin.site.register(Language)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
@@ -24,4 +26,13 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
