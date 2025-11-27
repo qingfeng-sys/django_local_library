885f5b7d3bedbbd95cd27e4b4572037bb95803be
qingfeng-sys <gy930309@gmail.com>
2025-11-27 23:58:44 +0800

    对关联记录进行在线编辑

--- catalog/admin.py
+++ catalog/admin.py
@@ -18,10 +18,15 @@ class AuthorAdmin(admin.ModelAdmin):
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
