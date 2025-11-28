9feaa612fff6e658a639352d7024a5cbf1bb69df
qingfeng-sys <gy930309@gmail.com>
2025-11-29 00:13:31 +0800

    纠正模板路径

--- catalog/views.py
+++ catalog/views.py
@@ -24,4 +24,4 @@ def index(request):
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, 'catalog/index.html', context=context)
